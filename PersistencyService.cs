c558cdc784d99d21071e59fd5a7f8aca7aa00bd8
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-05-18 11:34:11 +0200

    Lavede gemdata persistency service

--- PersistencyService.cs
+++ PersistencyService.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace FlytteFirmaBestillingsKlient
 {
     class PersistencyService
     {
+        private static string filNavn = "Booking1.json";
 
+        public static async Task GemDataTilDiskAsyncPS(Booking booking)
+        {
+            string jsonText = GetJsonPS(booking);
+            StorageFolder localfolder = ApplicationData.Current.LocalFolder;
+
+            StorageFile file = await localfolder.CreateFileAsync(filNavn, CreationCollisionOption.ReplaceExisting);
+
+            await FileIO.WriteTextAsync(file, jsonText);
+        }
+
+        public static string GetJsonPS(Booking booking)
+        {
+            string json = JsonConvert.SerializeObject(booking);
+            return json;
+        }
     }
 }
