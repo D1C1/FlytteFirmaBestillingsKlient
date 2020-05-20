f7e8ba882d39c6816274aef15357663326856447
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-05-20 12:32:19 +0200

    sat adresse klasse op med properties og constructor

--- Adresse.cs
+++ Adresse.cs
@@ -15,5 +15,17 @@ namespace FlytteFirmaBestillingsKlient
         int _antalVærelser;
         int _parkeringsAfstandmeter;
         bool _isElevator;
+
+        public Adresse()
+        {
+
+        }
+
+        public string Adresselinje { get => _adresselinje; set => _adresselinje = value; }
+        public int Postnummer { get => _postnummer; set => _postnummer = value; }
+        public string By { get => _by; set => _by = value; }
+        public int AntalVærelser { get => _antalVærelser; set => _antalVærelser = value; }
+        public int ParkeringsAfstandmeter { get => _parkeringsAfstandmeter; set => _parkeringsAfstandmeter = value; }
+        public bool IsElevator { get => _isElevator; set => _isElevator = value; }
     }
 }
