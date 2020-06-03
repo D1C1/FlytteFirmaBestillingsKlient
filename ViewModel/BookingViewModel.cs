9f26b4898c4fe99fb84e18847db238b686d33c9c
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-06-03 13:04:46 +0200

    tilføjet nyt advarsels system til brugeren af systemet i tilfælde af at  kunden mangler at indtaste data, implementerede inotifypropertychanged så vi kan fortælle view at advarsel property er ændret

--- ViewModel/BookingViewModel.cs
+++ ViewModel/BookingViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +11,11 @@ namespace FlytteFirmaBestillingsKlient
     /// <summary>
     /// Denne klasse er den viewmodel som bliver brugt til bestilling af en booking
     /// </summary>
-    public class BookingViewModel
+    public class BookingViewModel : INotifyPropertyChanged
     {
+        private string _advarsler;
+        private bool canSave = false;// tjekker om vi har alle de nødvendige data for at gemme til disk
+
         /// <summary>
         /// Constructor som initiere noget testdata samt en relaycommand til knapper på vores view
         /// </summary>
@@ -23,45 +28,77 @@ namespace FlytteFirmaBestillingsKlient
         //Properties
         public Booking CurrentBooking { get; set; } = new Booking();
         public RelayCommand GemDataButton { get; set; }
-        //Brugerdetaljer start
-        public string NavnView { get; set; }
-        public string EmailView { get; set; }
-        public int TlfnrView { get; set; }
-        //Brugerdetaljer slut
+
+        public string Advarsler
+        {
+            get { return _advarsler; }
+            set
+            {
+                _advarsler = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public bool CanSave { get => canSave; set => canSave = value; }
         public DateTimeOffset MovingDateView { get; set; }// Flyttedato sat af calender date picker
         //Properties slut
+        
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         /// <summary>
         /// Sætter informationen som bliver indtastet i vores view til den gemte booking objekt, samt henter adresser fra midlertidig booking som bliver lavet i adresseview
         /// </summary>
-        private async void SetInformation()
+        private async Task SetInformation()
         {
+            try
+            {
+                Booking tempbooking = await PersistencyService.HentBooking("tempbooking.json");
+                if (tempbooking==null)
+                {
+                    Advarsler = "Adresse ikke sat, gå venligst til adresse side for at sætte adresse";
+                    CanSave = false;
+                }
+                else
+                {
+                    CurrentBooking.AdresseFra = tempbooking.AdresseFra;
+                    CurrentBooking.AdresseTil = tempbooking.AdresseTil;
+                    CanSave = true;
+                } 
+                
+            }
+            catch (Exception)
+            {
+                Advarsler = "Adresse ikke sat, gå venligst til adresse side for at sætte adresse";
+                CanSave = false;
+            }
 
-            Booking tempbooking = await PersistencyService.HentBooking("tempbooking");
-            CurrentBooking.AdresseFra = tempbooking.AdresseFra;
-            CurrentBooking.AdresseTil = tempbooking.AdresseTil;
-            CurrentBooking.BookingBruger.ForNavn = NavnView;
-            CurrentBooking.BookingBruger.Email = EmailView;
-            CurrentBooking.BookingBruger.TlfNr = TlfnrView;
             CurrentBooking.FlytteDato = MovingDateView.Date;
         }
-        
+
         /// <summary>
         /// Gemmer vores booking objekt til en liste på vores disk, hvis listen ikke findes laver den først listen og prøver igen
         /// </summary>
         private async void GemDataTilDiskAsync()
         {
-            SetInformation();
-            try
+            await SetInformation();
+            if (CanSave)
             {
-                await PersistencyService.GemDataTilDiskAsyncPS(CurrentBooking);
+                try
+                {
+                    await PersistencyService.GemDataTilDiskAsyncPS(CurrentBooking);
+                    Advarsler = "gem booking succes, sendt til admin side";
+                }
+                catch (Exception)
+                {
+                    PersistencyService.Makefile("Booking1.json");
+                    await PersistencyService.GemDataTilDiskAsyncPS(CurrentBooking);
+                    Advarsler = "gem booking succes sendt til admin side";
+                }
             }
-            catch (Exception)
-            {
-                PersistencyService.Makefile("Booking1.json");
-                await PersistencyService.GemDataTilDiskAsyncPS(CurrentBooking);
-            }
-            
+
         }
 
     }
